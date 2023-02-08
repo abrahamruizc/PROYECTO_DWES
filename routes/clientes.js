7f6fce39d47f9d71ff2f4336b9aa24461b089f45
ismaelsevidanes <alum.isevidanesd@iesalixar.org>
2023-02-08 18:40:42 +0100

    CRUD Básicos Cliente

--- routes/clientes.js
+++ routes/clientes.js
@@ -9,24 +9,24 @@ var db = mongoose.connection;
 
 // GET del listado de usuarios ordenados por fecha de creación
 router.get('/', function(req, res, next) {
-  Cliente.find().sort('-creationdate').exec(function(err, users) {
+  Cliente.find().sort('-creationdate').exec(function(err, clients) {
   if (err) res.status(500).send(err);
-  else res.status(200).json(users);
+  else res.status(200).json(clients);
   });
   });
 
 // GET de un único usuario por su Id
 router.get('/:id', function(req, res, next) {
-  Cliente.findById(req.params.id, function(err, userinfo) {
+  Cliente.findById(req.params.id, function(err, clientinfo) {
   if (err) res.status(500).send(err);
-  else res.status(200).json(userinfo);
+  else res.status(200).json(clientinfo);
   });
   });
 
 
 // POST de un nuevo usuario
 router.post('/', function(req, res, next) {
-  Cliente.create(req.body, function(err, userinfo) {
+  Cliente.create(req.body, function(err, clientinfo) {
   if (err) res.status(500).send(err);
   else res.sendStatus(200);
   });
@@ -36,17 +36,18 @@ router.post('/', function(req, res, next) {
 // PUT de un usuario existente identificado por su Id
 router.put('/:id', function(req, res, next) {
   Cliente.findByIdAndUpdate(req.params.id, req.body, function(err,
-  userinfo) {
+    clientinfo) {
   if (err) res.status(500).send(err);
   else res.sendStatus(200);
   });
   });
 
 // DELETE de un usuario existente identificado por su Id
-router.delete('/:id', function(req, res, next) {
-  Cliente.findByIdAndDelete(req.params.id, function(err, userinfo) {
-  if (err) res.status(500).send(err);
-  else res.sendStatus(200);
+
+router.delete("/borrar:id", function (req, res, next) {
+  Cliente.findByIdAndDelete(req.params.id, function (err, clientinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
   });
-  });
+});
 module.exports = router;
