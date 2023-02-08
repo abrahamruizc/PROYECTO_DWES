90fe88d59b7f4bc24e9f2ed7b6e53a30ad69b6d3
ismaelsevidanes <alum.isevidanesd@iesalixar.org>
2023-02-08 17:23:34 +0100

    Peticiones CRUD Cliente

--- routes/clientes.js
+++ routes/clientes.js
@@ -6,9 +6,47 @@ var mongoose = require('mongoose');
 var Cliente= require('../models/Cliente');
 var db = mongoose.connection;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
+// GET del listado de usuarios ordenados por fecha de creación
+router.get('/', function(req, res, next) {
+  Cliente.find().sort('-creationdate').exec(function(err, users) {
+  if (err) res.status(500).send(err);
+  else res.status(200).json(users);
+  });
+  });
+
+// GET de un único usuario por su Id
+router.get('/:id', function(req, res, next) {
+  Cliente.findById(req.params.id, function(err, userinfo) {
+  if (err) res.status(500).send(err);
+  else res.status(200).json(userinfo);
+  });
+  });
+
+
+// POST de un nuevo usuario
+router.post('/', function(req, res, next) {
+  Cliente.create(req.body, function(err, userinfo) {
+  if (err) res.status(500).send(err);
+  else res.sendStatus(200);
+  });
+  });
+
+
+// PUT de un usuario existente identificado por su Id
+router.put('/:id', function(req, res, next) {
+  Cliente.findByIdAndUpdate(req.params.id, req.body, function(err,
+  userinfo) {
+  if (err) res.status(500).send(err);
+  else res.sendStatus(200);
+  });
+  });
+
+// DELETE de un usuario existente identificado por su Id
+router.delete('/:id', function(req, res, next) {
+  Cliente.findByIdAndDelete(req.params.id, function(err, userinfo) {
+  if (err) res.status(500).send(err);
+  else res.sendStatus(200);
+  });
+  });
 module.exports = router;
