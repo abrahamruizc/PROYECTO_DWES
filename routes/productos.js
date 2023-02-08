7c69161f4fa507f08cd7e595403ef852226086a5
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-02-08 17:19:52 +0100

    peticiones basicas.1

--- routes/productos.js
+++ routes/productos.js
@@ -1,13 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var mongoose = require('mongoose');
-var Producto = require('../models/Producto');
+var mongoose = require("mongoose");
+var Producto = require("../models/Producto");
 var db = mongoose.connection;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/*PETICION GET DE TODOS LOS PRODUCTOS(SIN FILTROS)*/
+router.get("/", function (req, res, next) {
+  Producto.find().exec(function (err, producs) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(producs);
+  });
+});
+
+router.post("/", function (req, res, next) {
+  Producto.create(req.body, function (err, productinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+router.delete("/borrar:id", function (req, res, next) {
+  Producto.findByIdAndDelete(req.params.id, function (err, productinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
 });
 
 module.exports = router;
