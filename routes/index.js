abff2bb791771e3c36ce57350006e244a6758f7a
DavidMartinezMerencioAlixar <alum.dmartinezm.2022@iesalixar.org>
2023-03-09 16:56:31 +0100

    POST Venta validaciones

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Plantitas_SL' });
 });
 
-/* GET del layout validacionde producto */
+/* GET del layout validacion de producto */
 router.get('/productos.ejs', function(req, res, next) {
   res.render('productos', { title: 'Plantitas_SL', layout: './productos.ejs' });
 });
@@ -14,6 +14,11 @@ router.get('/productos.ejs', function(req, res, next) {
 /* GET del layout validaciones cliente */
 router.get('/clientes.ejs', function(req, res, next) {
   res.render('clientes', { title: 'Plantitas_SL', layout: './clientes.ejs' });
+});
+
+/* GET del layout validaciones Venta */
+router.get('/ventas.ejs', function(req, res, next) {
+  res.render('ventas', { title: 'Plantitas_SL', layout: './ventas.ejs' });
 });
 
 module.exports = router;
--- routes/ventas.js
+++ routes/ventas.js
@@ -1,10 +1,16 @@
 
 var express = require('express');
+var app = express();
 var router = express.Router();
 
 var mongoose = require('mongoose');
 var Venta = require('../models/Venta');
 var db = mongoose.connection;
+
+const { body, validationResult } = require('express-validator');
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 // GET por defecto. Si tiene una query de Cliente, Empleado o Fecha,
 // se buscará aplicando dicho filtro. Si no, devolverá todas las Ventas
@@ -19,35 +25,55 @@ router.get("/", function (req, res, next) {
   } else if (fecha) {
     query.fecha = fecha;
   }
-  
+
   Venta.find(query)
     .sort("-creationdate")
     .populate("cliente")
     .populate("empleado")
     .exec(function (err, ventas) {
-    if (err) res.status(500).send(err);
-    else res.status(200).json(ventas);
-  });
+      if (err) res.status(500).send(err);
+      else res.status(200).json(ventas);
+    });
 });
 
 // GET de una única Venta por su ID
 router.get("/:id", function (req, res, next) {
   Venta.findById(req.params.id)
-  .sort("-creationdate")
-  .populate("cliente")
-  .populate("empleado")
-  .exec(function (err, ventas) {
-    if (err) res.status(500).send(err);
-    else res.status(200).json(ventas);
-  });
+    .sort("-creationdate")
+    .populate("cliente")
+    .populate("empleado")
+    .exec(function (err, ventas) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(ventas);
+    });
 });
 
 // POST de una nueva Venta
-router.post("/", function (req, res, next) {
-  Venta.create(req.body, function (err, venta) {
-    if (err) res.status(500).send(err);
-    else res.sendStatus(200);
-  });
+router.post("/", [
+  body('numero_venta', 'Ingresa un nº de venta válido').exists().isNumeric(),
+  body('fecha', 'Ingresa una fecha válida').exists().isDate(),
+  body('cliente', 'Ingresa un ID de cliente válido').exists(),
+  body('empleado', 'Ingresa un ID de empleado válido').exists(),
+  body('producto', 'Ingresa un producto válido').exists().isLength({ min: 8 }),
+  body('cantidad', 'Ingresa una cantidad válida').exists().isNumeric(),
+  body('precio', 'Ingresa un precio válido').exists().isNumeric()
+], function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    Venta.create({
+      numero_venta: req.body.numero_venta,
+      fecha: req.body.fecha,
+      cliente: req.body.cliente,
+      empleado: req.body.empleado,
+      linea: {
+        producto: req.body.producto,
+        cantidad: req.body.cantidad,
+        precio: req.body.precio
+      }
+    }).then(Venta => res.json(Venta)
+    ).catch(error => res.json(error));
 });
 
 // PUT de una Venta por su ID
